a1c5401e61d752795ee70be6aea832cda5f25c2b
kadek <kadek@gotomalls.com>
2022-02-18 19:42:16 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading"> 
+    <DataTitle :text="title" :dataDate="dataDate"/>
+    <DataBoxes :stats="stats" />
+  </main>
+
+  <main v-else class="flex flex-col align-center justify-center text-center"> 
+    <div class="text-gray-500 text-3xl mt-10 mb-6">fetching data...</div>
+    <div class="fa-3x"><i class="fas fa-spinner fa-pulse"></i></div>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DataTitle from '@/components/DataTitle'
+import DataBoxes from '@/components/DataBoxes'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    DataTitle,
+    DataBoxes,
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: '',
+    }
+  },
+  methods: {
+    async fetchData() {
+      const response = await fetch('https://api.covid19api.com/summary')
+      const data = await response.json()
+      return data
+    }
+  },
+  async created() {
+    const data = await this.fetchData()
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
   }
 }
 </script>
