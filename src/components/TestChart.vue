bedc141874ad614afaa83eab964bcd551334d8b8
kadek <kadek@gotomalls.com>
2022-02-19 20:36:31 +0800

    add chartjs with sample chart

---
+++ src/components/TestChart.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+</template>
+
+<script>
+import { Line } from 'chart.js'
+import { Chart, registerables } from 'chart.js';
+
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+};
+
+const config = {
+  type: 'line',
+  data,
+  options: {}
+};
+
+export default {
+  extends: Line,
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted () {
+      var myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+  }
+}
+</script>
---
+++ src/views/Chart.vue
@@ -0,0 +1,32 @@
+<template>
+  <TestChart :chartdata="chartData" :options="chartOptions"/>
+</template>
+
+<script>
+import TestChart from '@/components/TestChart'
+
+export default {
+  name: 'Chart',
+  components: {
+    TestChart
+  },
+  data() {
+    return {
+      chartdata: {
+      labels: ['January', 'February'],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#f87979',
+          data: [40, 20]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+    }
+  }
+}
+</script>
