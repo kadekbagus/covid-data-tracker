976df80725e6f7b7d7b75afee251304bb58744be
kadek <kadek@gotomalls.com>
2022-02-22 14:56:54 +0800

    add contact page and fix menu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <!-- <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>|
-    <router-link to="/test-chart">Test Chart</router-link>
-  </div> -->
-
   <Header/>
   <div class="container">
     <router-view/>
