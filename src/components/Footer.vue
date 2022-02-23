d339dd9e9cd8df74ddfe4c9b8156b0ab815c8294
kadek <kadek@gotomalls.com>
2022-02-23 23:08:31 +0800

    fix footer wording

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -77,45 +77,44 @@
               d="M488.6 250.2L392 214V105.5c0-15-9.3-28.4-23.4-33.7l-100-37.5c-8.1-3.1-17.1-3.1-25.3 0l-100 37.5c-14.1 5.3-23.4 18.7-23.4 33.7V214l-96.6 36.2C9.3 255.5 0 268.9 0 283.9V394c0 13.6 7.7 26.1 19.9 32.2l100 50c10.1 5.1 22.1 5.1 32.2 0l103.9-52 103.9 52c10.1 5.1 22.1 5.1 32.2 0l100-50c12.2-6.1 19.9-18.6 19.9-32.2V283.9c0-15-9.3-28.4-23.4-33.7zM358 214.8l-85 31.9v-68.2l85-37v73.3zM154 104.1l102-38.2 102 38.2v.6l-102 41.4-102-41.4v-.6zm84 291.1l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6zm240 112l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6z">
             </path>
           </svg>
-          Tailwind ELEMENTS
+          Covid Data Tracker
         </h6>
         <p>
-          Here you can use rows and columns to organize your footer content. Lorem ipsum dolor
-          sit amet, consectetur adipisicing elit.
+          Covid data tracker rely on external API, in the future this app may no longer work if the API shutdown or restricted.
         </p>
       </div>
       <div class="">
         <h6 class="uppercase font-semibold mb-4 flex justify-center md:justify-start">
-          Products
+          Remember
         </h6>
         <p class="mb-4">
-          <a href="#!" class="text-gray-600">Angular</a>
+          <a href="#!" class="text-gray-600">Always</a>
         </p>
         <p class="mb-4">
-          <a href="#!" class="text-gray-600">React</a>
+          <a href="#!" class="text-gray-600">Wear</a>
         </p>
         <p class="mb-4">
-          <a href="#!" class="text-gray-600">Vue</a>
+          <a href="#!" class="text-gray-600">Mask</a>
         </p>
         <p>
-          <a href="#!" class="text-gray-600">Laravel</a>
+          <a href="#!" class="text-gray-600">Wherever you go</a>
         </p>
       </div>
       <div class="">
         <h6 class="uppercase font-semibold mb-4 flex justify-center md:justify-start">
-          Useful links
+          If You Got Sick
         </h6>
         <p class="mb-4">
-          <a href="#!" class="text-gray-600">Pricing</a>
+          <a href="#!" class="text-gray-600">Go</a>
         </p>
         <p class="mb-4">
-          <a href="#!" class="text-gray-600">Settings</a>
+          <a href="#!" class="text-gray-600">See</a>
         </p>
         <p class="mb-4">
-          <a href="#!" class="text-gray-600">Orders</a>
+          <a href="#!" class="text-gray-600">The</a>
         </p>
         <p>
-          <a href="#!" class="text-gray-600">Help</a>
+          <a href="#!" class="text-gray-600">Doctor</a>
         </p>
       </div>
       <div class="">
@@ -129,7 +128,7 @@
               d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z">
             </path>
           </svg>
-          New York, NY 10012, US
+          Bongan Puseh, Kab.Tabanan, Bali
         </p>
         <p class="flex items-center justify-center md:justify-start mb-4">
           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope"
@@ -165,8 +164,8 @@
     </div>
   </div>
   <div class="text-center p-6 bg-gray-200">
-    <span>© 2021 Copyright:</span>
-    <a class="text-gray-600 font-semibold" href="https://tailwind-elements.com/">Tailwind Elements</a>
+    <span>© 2022 Copyright: </span>
+    <a class="text-gray-600 font-semibold" href="https://kadekbagus.github.io/">Kadek Bagus</a>
   </div>
 </footer>
 
